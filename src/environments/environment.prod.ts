dafc035fe0e0a7da1bb029e0eb7bc842b5fdf529
Daniel Powell <danieljtp@gmail.com>
2019-03-02 08:21:58 -0500

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDGiZI6MflRF63Xy-Us5GQZOPY0Jj8E57o",
+    authDomain: "scatter-16ce3.firebaseapp.com",
+    databaseURL: "https://scatter-16ce3.firebaseio.com",
+    projectId: "scatter-16ce3",
+    storageBucket: "scatter-16ce3.appspot.com",
+    messagingSenderId: "850406906935"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,17 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDGiZI6MflRF63Xy-Us5GQZOPY0Jj8E57o",
+    authDomain: "scatter-16ce3.firebaseapp.com",
+    databaseURL: "https://scatter-16ce3.firebaseio.com",
+    projectId: "scatter-16ce3",
+    storageBucket: "scatter-16ce3.appspot.com",
+    messagingSenderId: "850406906935"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
