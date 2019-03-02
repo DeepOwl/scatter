f53ee90ce7da3302c1b400cf28220aa332a549e1
Daniel Powell <danieljtp@gmail.com>
2019-03-02 14:30:23 -0500

    redirect direclty to scatter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { ScatterComponent } from './scatter/scatter.component';
 import { ScatterGameComponent } from './scatter-game/scatter-game.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/scatter', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'scatter/:id', component: ScatterGameComponent },
   { path: 'scatter', component: ScatterComponent }
